e46154d23fa2dedd1684d1020b2215e02eec1e2a
龚雪婷 <gongxueting.sh@chinaccs.cn>
2023-08-26 10:10:28 +0800

    js联系

---
+++ js练习/825.js
@@ -0,0 +1,47 @@
+// 给你一个整数数组 nums 。数组中唯一元素是那些只出现恰好一次的元素。
+// 请你返回 nums 中唯一元素的 和 。
+// 输入：nums = [1,2,3,2]
+// 输出：4
+// 解释：唯一元素为 [1,3] ，和为 4 。
+function singleDataSum(data){
+  if(!data||(data&&!data.length)) return 0
+
+  const map = new Map()
+  let tmp = []
+  data.forEach(item=>{
+    if(map.has(item)) {
+      map.set(item,map.get(item)+1)
+    }else{
+      map.set(item,1)
+    }
+  })
+  for(const [key,value] of map){
+    if(value==1){
+      tmp.push(key)
+    }
+  }
+  return tmp.reduce((pre,current)=>pre+current,0)
+}
+
+// 实现一个call
+// fun.call(this,1,2,3)
+
+function myCall(context,...rest){
+  const key = new Symbol()
+  context[key] = this;
+  const res = context[key](...rest)
+  delete context[key]
+  return res
+}
+// bind
+
+function myBind(context,...initArg){
+  const fun = this;
+  let bindFun = function(...rest){
+    if(this instanceof bindFun){
+      return new foo(...initArg,...rest)
+    }
+    return fun.call(context,...initArg,...rest)
+  }
+  return bindFun
+}
---
+++ react/helloReact.js
@@ -0,0 +1,51 @@
+// 创建、嵌套组件
+// 添加标签样式
+// 显示数据
+// 渲染条件列表
+// 对事件做出响应
+// 组件间数据共享
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
+// react的组件必须是大写 html标签必须是小写
+function MyButton(){
+  return(
+    <>
+    <button onClick>hello react</button>
+    </>
+  )
+}
+let content;
+if(user.name){
+  content = <AdminPanel/>
+}else{
+  content = <LoginPanel/>
+}
+function MyApp(){
+  return(
+    <>
+    <h1>welcome to my app</h1>
+    <MyButton/>
+    <p className = 'myP'>this is a p tag
+     <span>{user.name}</span>
+    </p>
+     <div>
+      {content}
+      <img src = {user.imageUrl} 
+           style = {{width:user.imageSize,height:user.imageSize}}/>
+     </div>
+     <ul>
+       {products.map(item=><li key={item.id}>{item.title}</li>)}
+     </ul>
+    </>
+  )
+}
